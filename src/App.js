62898e7cf66b449175702cb071168a4a2a21b322
btd1337 <helder.bertoldo@ice.ufjf.br>
2017-12-21 19:50:00 -0200

    0.0.3 Criado objeto de estado da aplicação

--- src/App.js
+++ src/App.js
@@ -10,13 +10,30 @@ class App extends Component {
   constructor () {
     super();
     this.state = {
-
+      userInfo: {
+        userName: 'Helder Bertoldo',
+        repos: 12,
+        followers: 10,
+        following: 10
+      },
+      repos: [{
+        name: 'Repo',
+        link: '#'
+      }],
+      starred: [{
+        name: 'Repo',
+        link: '#'
+      }]
     }
   }
 
   render() {
     return (
-      <AppContent />
+      <AppContent 
+        userInfo={this.state.userInfo}
+        repos={this.state.repos}
+        starred={this.state.starred}
+      />
     );
   }
 }
--- src/components/app-content.js
+++ src/components/app-content.js
@@ -1,38 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import Actions from './actions';
 import Repos from './repos';
 import Search from './search';
 import UserInfo from './user-info';
 
-const AppContent = () => (
+const AppContent = ({userInfo, repos, starred}) => (
 	<div className='box'>
 	<div className='col-md-4'></div>
 	<div className='app col-md-4'>
 	  <Search />
-	  <UserInfo />
-	  <Actions />
-	  <Repos 
+	  {!!userInfo && <UserInfo userInfo={userInfo}/>}
+	  {!!userInfo && <Actions />}
+	  {!!repos.length && <Repos 
 		className='repos' 
 		title='Repositórios'
-		repos={[
-		  {
-			name: 'Nome do Repositório',
-			link: '#'
-		  }
-		]} 
-	  />
-	  <Repos 
+		repos={repos} 
+	  />}
+	  {!!starred.length && <Repos 
 		className='starred' 
 		title='Favoritos'
-		repos={[
-		  {
-			name: 'Nome do Repositório',
-			link: '#'
-		  }
-		]} 
-	  />
+		repos={starred} 
+	  />}
 	</div>
   </div>
 )
+
+AppContent.propTypes = {
+	userInfo: PropTypes.object.isRequired,
+	repos: PropTypes.array.isRequired,
+	starred: PropTypes.array.isRequired
+}
 
 export default AppContent;
